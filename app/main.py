d2abd2ee3291dcc9199b48187a3ed46b4337e441
jeremyfouquet <91273814+jeremyfouquet@users.noreply.github.com>
2023-02-03 11:35:51 +0100

    module sauvegarde

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from extraire import extraire
 from analyser import analyser
+from sauvegarder import sauvegarder
 
 class main:
     value = -1
@@ -14,12 +15,16 @@ class main:
         label.pack(pady=20)
         btn1 = Button(text='Créer/Extraire un corpus de compétences', command=lambda:choix(1))
         btn1.pack(pady=20)
-        btn2 = Button(text='Analyser/Évaluer un corpus de compétences', command=lambda:choix(2))
+        btn2 = Button(text='Sauvergarder la base de données au fromat CSV', command=lambda:choix(2))
         btn2.pack(pady=20)
+        btn3 = Button(text='Analyser/Évaluer un corpus de compétences', command=lambda:choix(3))
+        btn3.pack(pady=20)
         root.mainloop()
         if main.value == 1:
             extraire.commencer()
         elif main.value == 2:
+            sauvegarder.commencer()
+        elif main.value == 3:
             analyser.commencer()
         else :
             exit(0)
